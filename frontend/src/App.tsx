27511347aa576f9599a526b7be4cc0687016bc93
Ryan <rfadaiia@uci.edu>
2026-07-20 11:18:50 -0700

    Frontend updates: Updated Style, Added Username text box, Play button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return <div className="App">Hello World</div>
+const App: React.FC = () => {
+  const [username, setUsername] = useState("");
+
+  return <div className="App">
+
+    <span className="heading">Prompnesia</span>
+
+    <label className="username-field">
+      <input
+        type="text"
+        name="username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+        placeholder="Enter your username"
+        autoComplete="username"
+      />
+    </label>
+
+    <button className="play-button" type="button">
+      Play!
+    </button>
+
+  </div>
+
 }
 
 export default App;
